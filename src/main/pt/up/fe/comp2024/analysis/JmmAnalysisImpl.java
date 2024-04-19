64e74aad21a93c2dbf3d70a18ad57c13136964f0
Luís Du <up202105385@up.pt>
2024-04-19 12:29:07 +0100

    adding semantic tests

--- src/main/pt/up/fe/comp2024/analysis/JmmAnalysisImpl.java
+++ src/main/pt/up/fe/comp2024/analysis/JmmAnalysisImpl.java
@@ -20,8 +20,7 @@ public class JmmAnalysisImpl implements JmmAnalysis {
     private final List<AnalysisPass> analysisPasses;
 
     public JmmAnalysisImpl() {
-        //this.analysisPasses = List.of(new DuplicateVerifier(), new MethodVerifier(), new VarargsVerifier(), new DeclarationVerifier(), new TypeVerifier(), new ArrayVerifier());
-        this.analysisPasses = List.of(new BruteForce());
+        this.analysisPasses = List.of(new DuplicateVerifier(), new MethodVerifier(), new VarargsVerifier(), new DeclarationVerifier(), new TypeVerifier(), new ArrayVerifier());
     }
 
     @Override
